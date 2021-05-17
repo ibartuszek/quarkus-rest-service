0970db2550c4ced7fe67dc2af85542ceac06abc0
ibartuszek <istvan.bartuszek@gmail.com>
2021-05-17 17:39:27 +0200

    kafka: add some logging, groupId, offsetReset policy and log retention minutes

--- kafka-quickstart/src/main/java/org/acme/kafka/fruit/FruitProcessor.java
+++ kafka-quickstart/src/main/java/org/acme/kafka/fruit/FruitProcessor.java
@@ -3,6 +3,8 @@ package org.acme.kafka.fruit;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -10,11 +12,13 @@ import javax.enterprise.context.ApplicationScoped;
 public class FruitProcessor {
 
     private static final double CONVERSION_RATE = 0.88;
+    private static final Logger log = LoggerFactory.getLogger(FruitProcessor.class);
 
-     @Incoming("fruit-in")
-     @Outgoing("fruit-out")
-     @Broadcast
+    @Incoming("fruit-in")
+    @Outgoing("fruit-out")
+    @Broadcast
     public Fruit process(Fruit fruit) {
+        log.info("New fruit is under processing name={} original price={}", fruit.name, fruit.price);
         fruit.price = (int) (fruit.price * CONVERSION_RATE);
         return fruit;
     }
--- kafka-quickstart/src/main/java/org/acme/kafka/fruit/FruitResource.java
+++ kafka-quickstart/src/main/java/org/acme/kafka/fruit/FruitResource.java
@@ -3,6 +3,8 @@ package org.acme.kafka.fruit;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -13,6 +15,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/fruits")
 public class FruitResource {
 
+    private static final Logger log = LoggerFactory.getLogger(FruitResource.class);
+
     @Inject
     @Channel("fruit-out")
     Publisher<Fruit> fruits;
@@ -22,6 +26,7 @@ public class FruitResource {
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType(MediaType.APPLICATION_JSON)
     public Publisher<Fruit> stream() {
+        log.info("Stream...");
         return fruits;
     }
 
